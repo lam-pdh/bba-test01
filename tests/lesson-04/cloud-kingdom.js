3989c2fddffcd87a6d3c48105367ce3f46a34f46
test 01 - Lam Phan <lamphan290997@gmail.com>
2025-09-23 22:09:57 +0700

    feat: add solution for challenge 03

---
+++ tests/lesson-04/cloud-kingdom.js
@@ -0,0 +1,22 @@
+// Biến powerUp có giá trị là “mushroom”.
+let powerUp = "mushroom";
+let effect ;
+
+// Sử dụng câu điều kiện if...else để xác định tên hiệu ứng tương ứng với các powerUp:
+if (powerUp === "mushroom") {
+  effect = "Mario becomes Super!";
+}
+else if (powerUp === "flower") {
+  effect = "Mario can shoot fireballs!";
+}
+else if (powerUp === "star") {
+  effect = "Mario is invincible!";
+}
+else if (powerUp === "none") {
+  effect = "Mario is normal";
+} else {
+  effect = "Unknown power-up";
+}
+
+// In ra ngoài console hiệu ứng tên hiệu ứng
+console.log(effect);
